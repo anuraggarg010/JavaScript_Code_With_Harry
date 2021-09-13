f99319b4dc426f137b8ae9d6f449b14e20c9a23f
anuraggarg010 <anuraggarg096@gmail.com>
2021-09-13 22:13:42 +0530

    Update script.js

--- 16_Create_Remove_Replacing/script.js
+++ 16_Create_Remove_Replacing/script.js
@@ -21,11 +21,3 @@ elem2.className = 'elem2';
 let tnode = document.createTextNode('This is a created node for elem2');
 elem2.appendChild(tnode);
 
-element.replaceWith(elem2);
-let myul = document.getElementById('myul');
-myul.replaceChild(element, document.getElementById('fui'));
-myul.removeChild(document.getElementById('lui'));
-let pr = elem2.hasAttribute('href');
-elem2.removeAttribute('id');
-elem2.setAttribute('title', 'myelem2title');
-console.log(elem2, pr);
